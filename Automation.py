19f03699018e7faae7bc2ce634e37739539d755b
Whooshlie <112780809+Whooshlie@users.noreply.github.com>
2023-01-21 03:24:59 -0500

    Implement Basic Selemnium Automation

--- Automation.py
+++ Automation.py
@@ -1,61 +1,80 @@
-search = "Chess"
-search.replace(" ", "+")
 import time
 
 import chromedriver_autoinstaller
 from selenium import webdriver
+from selenium.webdriver import DesiredCapabilities
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import os
 
-
-
-
-opt = webdriver.ChromeOptions()
-opt.add_argument("--start-maximized")
-
-chromedriver_autoinstaller.install()
-driver = webdriver.Chrome(options=opt)
-driver.get('https://accounts.google.com/')
-
-wait = WebDriverWait(driver, 120)
 presence = EC.presence_of_element_located
 visible = EC.visibility_of_element_located
 
 
-while True:
-    url = driver.current_url
-    if url == "https://myaccount.google.com/?pli=1":
-        break
+class FeedChanger:
+    def __init__(self):
+        opt = webdriver.ChromeOptions()
+        opt.add_argument("--start-maximized")
+        ROOT_DIR = os.path.abspath(os.curdir)
 
-def likeWatchVideo(videoId: str, watch_time: int):
-    driver.get(f"https://www.youtube.com/watch?v={videoId}")
-    Xpath = "/html/body/ytd-app/div[1]/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/div[2]/ytd-watch-metadata/div/div[2]/div[2]/div/div/ytd-menu-renderer/div[1]/ytd-segmented-like-dislike-button-renderer/div[1]/ytd-toggle-button-renderer/yt-button-shape"
-    wait.until(visible((By.XPATH, Xpath)))
-    driver.find_element(By.XPATH, Xpath).click()
+        chromedriver_autoinstaller.install()
 
-    time.sleep(watch_time)
+        self.driver = webdriver.Chrome(options=opt)
+        self.wait = WebDriverWait(self.driver, 120)
 
 
 
+        """self.driver.get('https://accounts.google.com/')
 
 
-driver.get(f'https://www.youtube.com/results?search_query={search}')
-wait.until(visible((By.ID, "video-title")))
 
-driver.execute_script("window.scrollTo(0, 100000)")
+        while True:
+            url = self.driver.current_url
+            if url == "https://myaccount.google.com/?pli=1":
+                break"""
 
-source = driver.page_source
-print(source)
-ids = source.split('"videoId":"')[1:]
-for i, videoId in enumerate(ids):
-    ids[i] = videoId.split('"')[0]
-ids = set(ids)
-print(len(ids))
-#print(driver.find_element("video-title")[0])
-print(ids)
+    def likeWatchVideo(self, videoId: str, watch_time: int):
+        self.driver.get(f"https://www.youtube.com/watch?v={videoId}")
+        Xpath = "/html/body/ytd-app/div[1]/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/div[2]/ytd-watch-metadata/div/div[2]/div[2]/div/div/ytd-menu-renderer/div[1]/ytd-segmented-like-dislike-button-renderer/div[1]/ytd-toggle-button-renderer/yt-button-shape"
+        self.wait.until(visible((By.XPATH, Xpath)))
+        self.driver.find_element(By.XPATH, Xpath).click()
+        element = self.wait.until(EC.element_to_be_clickable((By.XPATH, "//*[@id='skip-button:6']/span/button")))
+        element.click()
 
-for videoId in ids:
-    likeWatchVideo(videoId, 1200)
 
+        time.sleep(watch_time)
+
+
+    def changeFeed(self, prompt, watchTime):
+        search = prompt
+        search = search.replace(" ", "+")
+
+
+
+
+
+
+
+        self.driver.get(f'https://www.youtube.com/results?search_query={search}')
+        self.wait.until(visible((By.ID, "video-title")))
+
+        self.driver.execute_script("window.scrollTo(0, 100000)")
+
+        source = self.driver.page_source
+        print(source)
+        ids = source.split('"videoId":"')[1:]
+        for i, videoId in enumerate(ids):
+            ids[i] = videoId.split('"')[0]
+        ids = set(ids)
+        print(len(ids))
+        #print(driver.find_element("video-title")[0])
+        print(ids)
+
+        for videoId in ids:
+            self.likeWatchVideo(videoId, watchTime)
+
+feedChanger = FeedChanger()
+feedChanger.likeWatchVideo("8tnf_LQTySM", 10)
+#feedChanger.changeFeed("Chess", 30)
